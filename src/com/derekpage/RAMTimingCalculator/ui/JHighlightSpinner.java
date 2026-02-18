0ff5269f44ef2ea661d8c073844ea97d747fd09a
derekpage3 <88250992+derekpage3@users.noreply.github.com>
2026-02-17 19:16:57 -0600

    Update JHighlightSpinner.java
    v 0.0.1

--- src/com/derekpage/RAMTimingCalculator/ui/JHighlightSpinner.java
+++ src/com/derekpage/RAMTimingCalculator/ui/JHighlightSpinner.java
@@ -1,11 +1,10 @@
 package com.derekpage.RAMTimingCalculator.ui;
 
-import com.derekpage.RAMTimingCalculator.logging.Logger;
-
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.FocusEvent;
 import java.awt.event.FocusListener;
+import java.awt.event.KeyListener;
 
 /**
  * An extension of the JSpinner class that changes the cell color when the spinner gains focus.
@@ -18,6 +17,9 @@ public class JHighlightSpinner extends JSpinner {
     //Keeps track of the original background color set by the runtime L&F.
     private Color originalBackgroundColor = null;
 
+    //Keeps track of the underlying text field used as the Editor for the JSpinner component.
+    private JFormattedTextField jtf = null;
+
     public JHighlightSpinner(Color highlightColor) {
         super();
         this.init(highlightColor);
@@ -26,18 +28,6 @@ public class JHighlightSpinner extends JSpinner {
     public JHighlightSpinner(SpinnerModel model, Color highlightColor) {
         super(model);
         this.init(highlightColor);
-        /*EEL listener = EEL.getInstance();
-//        listener.showDialog();
-        this.addFocusListener(listener);
-        this.addAncestorListener(listener);
-        this.addChangeListener(listener);
-        this.addVetoableChangeListener(listener);
-        this.addComponentListener(listener);
-        this.addContainerListener(listener);
-        this.addKeyListener(listener);
-        this.addInputMethodListener(listener);
-        this.addPropertyChangeListener(listener);
-        Logger.debug(this.getComponentCount());*/
     }
 
     /**
@@ -49,9 +39,44 @@ public class JHighlightSpinner extends JSpinner {
         if (highlightColor == null)
             throw new IllegalArgumentException("Parameter 'highlightColor' is required!");
         this.highlightColor = highlightColor;
-        this.originalBackgroundColor = this.getBackground();
-        this.addFocusListener(new JHighlightSpinner.HighlightFocusListener(this));
-        //TODO make this shit work.
+
+        //Get the editor.  Should never be null.
+        JComponent editor = this.getEditor();
+        if (editor == null)
+            return;
+
+        //Should always be an instance of JSpinner.DefaultEditor, but abort if not so
+        //NOTE: Not likely but it's a safety in case future versions of the JDK were to change it or reclass it.
+        if (!(editor instanceof JSpinner.DefaultEditor))
+            return;
+
+        //Get the underlying text field for the spinner's editor.
+        JFormattedTextField jtf = ((JSpinner.DefaultEditor) editor).getTextField();
+        if (jtf == null)
+            return;
+
+        //Capture the editor component and its original background color of the text field set by the L&F.
+        this.originalBackgroundColor = jtf.getBackground();
+        this.jtf = jtf;
+        jtf.addFocusListener(new JHighlightSpinner.HighlightFocusListener(this));
+
+
+        /*EEL listener = EEL.getInstance();
+        listener.showDialog();
+        this.addFocusListener(listener);
+        this.addAncestorListener(listener);
+        this.addChangeListener(listener);
+        this.addVetoableChangeListener(listener);
+        this.addComponentListener(listener);
+        this.addContainerListener(listener);
+        this.addKeyListener(listener);
+        this.addInputMethodListener(listener);
+        this.addPropertyChangeListener(listener);
+        this.addHierarchyBoundsListener(listener);
+        this.addHierarchyListener(listener);
+        this.addMouseListener(listener);
+        this.addMouseWheelListener(listener);
+        this.addMouseMotionListener(listener);*/
     }
 
     /// getter/setter ///
@@ -72,14 +97,40 @@ public class JHighlightSpinner extends JSpinner {
         this.originalBackgroundColor = originalBackgroundColor;
     }
 
-    /// Internal support FocusListner for custom functionality. ///
+    public JFormattedTextField getJtf() {
+        return jtf;
+    }
+
+    public void setJtf(JFormattedTextField jtf) {
+        this.jtf = jtf;
+    }
+
+
+    /// Event listener registration methods for the editor. ///
+
+    public synchronized void addEditorKeyListener(KeyListener l) {
+        this.jtf.addKeyListener(l);
+    }
+
+
+    public synchronized void removeEditorKeyListener(KeyListener l) {
+        this.jtf.removeKeyListener(l);
+    }
+
+
+    public synchronized KeyListener[] getEditorKeyListeners() {
+        return this.jtf.getKeyListeners();
+    }
+
+
+    /// Internal support FocusListener for custom functionality. ///
 
     /**
      * Implements the functionality needed to change to highlight color on focus and revert on losing focus.
      */
     protected static class HighlightFocusListener implements FocusListener {
 
-        //Keep reference to the JHighlighSpinner we're bound to.
+        //Keep reference to the JHighlightSpinner we're bound to.
         private JHighlightSpinner _spinner = null;
 
         public HighlightFocusListener(JHighlightSpinner spinner) {
@@ -90,14 +141,12 @@ public class JHighlightSpinner extends JSpinner {
 
         @Override
         public void focusGained(FocusEvent e) {
-            Logger.debug("Gained focus");
-//TODO            this._spinner.setBackground(this._spinner.getHighlightColor());
+            this._spinner.getJtf().setBackground(this._spinner.getHighlightColor());
         }
 
         @Override
         public void focusLost(FocusEvent e) {
-            Logger.debug("Lost focus");
-//TODO            this._spinner.setBackground(this._spinner.getOriginalBackgroundColor());
+            this._spinner.getJtf().setBackground(this._spinner.getOriginalBackgroundColor());
         }
     }
 }
